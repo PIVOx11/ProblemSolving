a117675612ad5de1b4b5a50c93564b374f9a5165
PIVOx <brahimhossam160@gmail.com>
2026-08-02 11:30:27 +0100

    Time: 0 ms (100%), Space: 19.2 MB (75.06%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,27 +1,27 @@
 class Solution:
     def stoneGame(self, p: List[int]) -> bool:
-        alice = 0
-        bob = 0
+        # alice = 0
+        # bob = 0
 
-        l = 0
-        r = len(p) - 1
+        # l = 0
+        # r = len(p) - 1
 
-        turn = 0
-        while l < r:
-            if p[l] > p[r]:
-                res = p[l]
-                l += 1
-            else:
-                r -= 1
-                res = p[r]
+        # turn = 0
+        # while l < r:
+        #     if p[l] > p[r]:
+        #         res = p[l]
+        #         l += 1
+        #     else:
+        #         r -= 1
+        #         res = p[r]
 
-            if turn == 0:
-                alice += res
-                turn = 1
-            else:
-                bob += res
-                turn = 0
+        #     if turn == 0:
+        #         alice += res
+        #         turn = 1
+        #     else:
+        #         bob += res
+        #         turn = 0
 
-            alice += p[l]        
+        #     alice += p[l]
     
-        return alice > bob
+        return True
