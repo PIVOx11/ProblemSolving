d8ae3f9e26c27137d785dfd384d3d1b4ef91008b
PIVOx <brahimhossam160@gmail.com>
2026-08-03 10:16:44 +0100

    Time: 1 ms (93.98%), Space: 20.1 MB (97.17%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,15 +1,30 @@
+# class Solution:
+#     def pivotIndex(self, n: List[int]) -> int:
+#         s = sum(n)
+#         l = len(n)
+#         arr = [0] * (l + 1)
+
+#         for i in range(l + 1):
+#             if i > 0:
+#                 arr[i] = arr[i - 1] + n[i - 1]
+
+#         for i in range(l):
+#             if ((s - n[i]) / 2) == arr[i]:
+#                 return i
+
+#         return -1
+
+
+
 class Solution:
-    def pivotIndex(self, n: List[int]) -> int:
-        s = sum(n)
-        l = len(n)
-        arr = [0] * (l + 1)
-
-        for i in range(l + 1):
-            if i > 0:
-                arr[i] = arr[i - 1] + n[i - 1]
-
-        for i in range(l):
-            if ((s - n[i]) / 2) == arr[i]:
-                return i
-
+    def pivotIndex(self, nums: List[int]) -> int:
+        total = sum(nums)
+        left = 0
+        
+        for index, value in enumerate(nums):
+            total -= value
+            if left == total:
+                return index
+            left += value
+        
         return -1
