13ac35f67b0cf1b0abc1a498c19567c04a21a5d9
PIVOx <brahimhossam160@gmail.com>
2026-08-04 19:42:13 +0100

    Time: 3 ms (69.59%), Space: 19.3 MB (55.74%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.py
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -1,5 +1,4 @@
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
         nums.sort()
-        n = set(nums)
-        return [i for i in range(nums[0]+1, nums[-1]) if i not in n]
+        return [i for i in range(nums[0]+1, nums[-1]) if i not in nums]
