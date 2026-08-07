11d4529222f114108478c5f6973922d98b3ea7f4
PIVOx <brahimhossam160@gmail.com>
2026-08-07 12:36:44 +0100

    Time: 0 ms (100%), Space: 19.4 MB (13.02%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+@lru_cache
+def seq(root: Optional[TreeNode], arr: Optional([tuple])) -> [tuple]:
+    if not arr:
+        arr = ()
+
+    if not root:
+        return arr
+
+    if not root.left and not root.right:
+        arr = (*arr, root.val)
+        return arr
+
+    return seq(root.left, arr) + seq(root.right, arr)
+
+
+class Solution:
+    def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        print(seq(root1, None))
+        print("---------------")
+        print(seq(root2, None))
+        return seq(root1, None) == seq(root2, None)
