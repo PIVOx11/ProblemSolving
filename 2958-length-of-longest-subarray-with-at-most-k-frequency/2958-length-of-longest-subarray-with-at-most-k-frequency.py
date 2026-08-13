803010707ff70c5bba02108abcb6cebc00f6120e
PIVOx <brahimhossam160@gmail.com>
2026-08-13 22:48:22 +0100

    Time: 183 ms (99.58%), Space: 35.3 MB (38.16%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.py
@@ -1,18 +1,20 @@
 class Solution:
     def maxSubarrayLength(self, n: List[int], k: int) -> int:
-        ans = 1
+        ans = 0
         f = defaultdict(int)
 
-        left = 0
+        right = left = 0
 
         for right in range(len(n)):
             v = n[right]
             f[v] += 1
 
-            while f[v] > k:
-                f[n[left]] -= 1
-                left += 1
-            ans = max(ans, (right + 1) - left)
-    
+            if f[v] > k:
+                ans = max(ans, right - left)
+                while f[v] > k:
+                    f[n[left]] -= 1
+                    left += 1
+
+        ans = max(ans, (right + 1) - left)
 
         return ans
