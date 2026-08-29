ac346155e40dd64ad82f7960165c702fd93fb530
PIVOx <brahimhossam160@gmail.com>
2026-08-29 13:41:54 +0100

    Time: 4 ms (12.12%), Space: 19.3 MB (74.26%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,11 +1,13 @@
 class Solution:
     def gcdOfStrings(self, s1: str, s2: str) -> str:
         l1, l2 = len(s1), len(s2)
-        ans = [""]
-        for i in range(1, l2 + 1):
+        
+        
+        for i in range(l2, 0, -1):
             part = s2[0: i]
             m1 , m2 = l1 // len(part), l2 // len(part)
 
             if part * m1 == s1 and part * m2 == s2:
-                ans.append(part)
-        return ans[-1]
+                return part
+        
+        return ""
