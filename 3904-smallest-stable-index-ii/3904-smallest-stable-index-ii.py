b56b8c59c673f73f4769217fad05c2518411457d
PIVOx <brahimhossam160@gmail.com>
2026-09-05 10:24:10 +0100

    Time: 137 ms (87.27%), Space: 32 MB (68.18%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
@@ -1,23 +1,17 @@
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
         l = len(nums)
-        preMax = [0] * l
-        sufMin = [nums[-1]] * l
-        for i, v in enumerate(nums):
-            if v > preMax[i - 1]:
-                preMax[i] = v
-            else:
-                preMax[i] = preMax[i - 1]
-        
-        for i, v in zip(range(l-1, -1, -1), nums[::-1]):
-            if i + 1 >= l:
-                continue
-            if nums[i] < sufMin[i + 1]:
-                sufMin[i] = nums[i]
-                continue
-            sufMin[i] = sufMin[i + 1]
-        # print(sufMin, preMax)
+        preMax = nums[0]
+        sufMin = [0] * l
+        mini = nums[-1]
+        for i in range(l-1, -1, -1):
+            if nums[i] < mini:
+                mini = nums[i]
+            sufMin[i] = mini
+        print(sufMin)
         for i in range(l):
-            if preMax[i] - sufMin[i] <= k:
+            if nums[i] > preMax:
+                preMax = nums[i]
+            if preMax - sufMin[i] <= k:
                 return i           
         return -1
