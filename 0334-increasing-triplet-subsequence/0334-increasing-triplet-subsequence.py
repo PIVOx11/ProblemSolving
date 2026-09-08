f442f64189cbbd94653715626f117d53de20d13d
PIVOx <brahimhossam160@gmail.com>
2026-09-08 17:56:05 +0100

    Time: 11 ms (95.31%), Space: 38.8 MB (67.24%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.py
@@ -3,26 +3,13 @@ class Solution:
         if len(n) <= 2 or len(set(n)) <= 2:
             return False
         
-        l = len(n)
-        i = 0
-        j = 1
-        k = 2
-
-        while k < l and j < l and i < l:
-            if n[i] < n[j] < n[k]:
+        i = j = float("inf")
+        for nb in n:
+            if nb <= i:
+                i = nb
+            elif nb <= j:
+                j = nb
+            else:
                 return True
-            
-            if n[i] >= n[j]:
-                i += 1
-                if j == i:
-                    j += 1
-                    if j == k:
-                        k += 1
-            elif n[k] <= n[j]:
-                k += 1
-            
-            if k == len(n):
-                j += 1
-                k = j + 1
-            
+
         return False
