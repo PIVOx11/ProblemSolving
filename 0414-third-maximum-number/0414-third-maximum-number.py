6418aeeda983b72b87aa1f4cfa75505c09958f3c
PIVOx <brahimhossam160@gmail.com>
2026-09-20 15:16:55 +0100

    Time: 0 ms (100%), Space: 19.7 MB (71.79%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -1,27 +1,44 @@
 class Solution:
-    def thirdMax(self, n: list[int]) -> int:
+    def thirdMax(self, nums: list[int]) -> int:
         # First Idea
         # n = sorted(list(set(n)))
 
         # return n[-3] if len(n) >= 3 else max(n)
         
         
-        n = set(n)
-        if len(n) < 3:
-            return max(n)
+        # n = set(n)
+        # if len(n) < 3:
+        #     return max(n)
         
-        t = max(n)
-        s = f = -float("inf")
+        # t = max(n)
+        # s = f = -float("inf")
 
-        for n in n:
-            if n > f:
-                f, s, t = n, f, s
-            elif n > s:
-                s, t = n, s
-            elif n > t:
-                t = n
+        # for n in n:
+        #     if n > f:
+        #         f, s, t = n, f, s
+        #     elif n > s:
+        #         s, t = n, s
+        #     elif n > t:
+        #         t = n
         
-        return t
+        # return t
 
-
+        # Sort the array.
+        nums.sort(reverse = True)
+        
+        elem_counted = 1
+        prev_elem = nums[0]
+        
+        for index in range(len(nums)):
+            # Current element is different from previous.
+            if nums[index] != prev_elem:
+                elem_counted += 1
+                prev_elem = nums[index]
+            
+            # If we have counted 3 numbers then return current number.
+            if elem_counted == 3:
+                return nums[index]
+        
+        # We never counted 3 distinct numbers, return largest number.
+        return nums[0]
 
